561c9b0b4b3b7c52c9e841129188148c85610050
Saamnuur <inuur65@gmail.com>
2021-06-21 17:53:38 +0000

    card flip coding added

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,10 +52,14 @@ document.addEventListener('DOMContentLoaded', () => {
             img: '../assets/images/solidcreamrainbow.png'
         }
     ]
+    
+    cardArray.sort (() => 0.5 Math.random())
 
     const grid = document.querySelector('.grid')
+    const resultsDisplay = document.querySelector('#result')
     var cardsChosen = []
     var cardsChosenId = []
+    var cardsWon = []
 
     //game board//
 
@@ -76,7 +80,20 @@ document.addEventListener('DOMContentLoaded', () => {
         var cards = document.querySelectorAll('img')
         const optionOneId = cardsChosenId[0]
         const optionTwoId = cardsChosenId[1]
-        if ()
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('Well done! You found a match')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', '../assets/images/locked.png')
+            cards[optionTwoId].setAttribute('src', '../assets/images/locked.png')
+            alert('Oops! Not a match! Try again.')
+        }
+        cardsChosen = []
+        cardsChosenId []
+        resultsDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultsDisplay.textContent = 'Congratulations! All the cards have been matched correctly!'
+        }
     }
 
 
