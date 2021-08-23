fa184de95f2ee496a3eaa720149834becca01e4d
Saamnuur <inuur65@gmail.com>
2021-08-23 21:20:55 +0000

    errors from jshint fixed by adding semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,67 +53,67 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   ];
 
-  cardArray.sort(() => 0.5 - Math.random())
+  cardArray.sort(() => 0.5 - Math.random());
 
-  const grid = document.querySelector('.grid')
-  const resultDisplay = document.querySelector('#result')
-  let cardsChosen = []
-  let cardsChosenId = []
-  let cardsWon = []
+  const grid = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
 
   //board creation//
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement('img')
-      card.setAttribute('src', '../assets/images/locked.png')
-      card.setAttribute('data-id', i)
-      card.addEventListener('click', flipCard)
-      grid.appendChild(card)
+      const card = document.createElement('img');
+      card.setAttribute('src', '../assets/images/locked.png');
+      card.setAttribute('data-id', i);
+      card.addEventListener('click', flipCard);
+      grid.appendChild(card);
     }
   }
 
   //check for matches//
   function checkForMatch() {
-    const cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
 
     if (optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', '../assets/images/locked.png')
-      cards[optionTwoId].setAttribute('src', '../assets/images/locked.png')
-      alert('You have clicked the same image!')
+      cards[optionOneId].setAttribute('src', '../assets/images/locked.png');
+      cards[optionTwoId].setAttribute('src', '../assets/images/locked.png');
+      alert('You have clicked the same image!');
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
-      alert('Well done! You found a match.')
-      cards[optionOneId].setAttribute('src', '../assets/images/unlocked.png')
-      cards[optionTwoId].setAttribute('src', '../assets/images/unlocked.png')
-      cards[optionOneId].removeEventListener('click', flipCard)
-      cards[optionTwoId].removeEventListener('click', flipCard)
-      cardsWon.push(cardsChosen)
+      alert('Well done! You found a match.');
+      cards[optionOneId].setAttribute('src', '../assets/images/unlocked.png');
+      cards[optionTwoId].setAttribute('src', '../assets/images/unlocked.png');
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
     } else {
-      cards[optionOneId].setAttribute('src', '../assets/images/locked.png')
-      cards[optionTwoId].setAttribute('src', '../assets/images/locked.png')
-      alert('Oopss! Sorry, try again.')
+      cards[optionOneId].setAttribute('src', '../assets/images/locked.png');
+      cards[optionTwoId].setAttribute('src', '../assets/images/locked.png');
+      alert('Oopss! Sorry, try again.');
     }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length / 2) {
-      resultDisplay.textContent = 'Congratulations! You have matched them all!'
+      resultDisplay.textContent = 'Congratulations! You have matched them all!';
     }
   }
 
   //flip the card//
   function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
     if (cardsChosen.length === 2) {
-      setTimeout(checkForMatch, 500)
+      setTimeout(checkForMatch, 500);
     }
   }
 
-  createBoard()
+  createBoard();
 });
 
